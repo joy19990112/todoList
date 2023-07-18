37292b260a6d30ec803fe2c7d9743d324ac8a7ed
Joy <mahongyu19990112@163.com>
2023-07-18 11:53:52 +0800

    feat: 按钮引用

--- src/view/main/components/todoFooter.vue
+++ src/view/main/components/todoFooter.vue
@@ -5,22 +5,33 @@
   3.删除
  -->
 <template>
+  <div>
+    <!-- 全选 -->
+    <van-button type="primary">全选</van-button>
+    <!-- 已做 -->
+    <van-button>已做</van-button>
+    <!-- 删除 -->
+    <van-button>删除</van-button>
+  </div>
 </template>
 
 <script>
+import { Button } from 'vant';
 export default {
-components: {},
-props: {},
-data() {
- return {
- };
-},
+  components: {
+    VanButton: Button
 
-created() {},
-mounted() {},
-methods: {},
-computed: {},
+  },
+  props: {},
+  data() {
+    return {
+    };
+  },
+
+  created() { },
+  mounted() { },
+  methods: {},
+  computed: {},
 }
 </script>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
